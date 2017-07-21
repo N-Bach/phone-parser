6cbbc9c965dc30737aa6250265c8d42935f98c0d
Nguyen Bach <nguyenbach2810@gmail.com>
2017-07-21 17:55:03 +0700

    add nyc for test coverage

--- server/utils/phoneParser.test.js
+++ server/utils/phoneParser.test.js
@@ -19,10 +19,20 @@ describe('phoneParser', () => {
   });
 
   it('should parse weird number', () => {
-    let phone = '0961234567';
     let s = `My phone number is 012437873935 0963787..  .39..35`;
     let result = phoneParser(s);
+    expect(result).to.have.lengthOf(0);
+  });
 
+  it('should parse number with space and dot in between', () => {
+    let s = `My phone number is 012437873935 0963787..  .39..35`;
+    let result = phoneParser(s);
+    expect(result).to.have.lengthOf(0);
+  });
+
+  it('should not parse phone number contain alphabetic character', () => {
+    let s = `My phone number is 012437tt873s93 0963787..  .3d9..3`;
+    let result = phoneParser(s);
     expect(result).to.have.lengthOf(0);
   });
 });
